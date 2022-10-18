15b5cd91009f8fb7758b71faed8bb1de472ef60b
Natyari <you@example.com>
2022-10-18 12:07:38 -0600

    update

--- BlazorServerSignalRApp/Hubs/ChatHub.cs
+++ BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -6,7 +6,7 @@ namespace BlazorServerSignalRApp.Server.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceuveMessage, user, message");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
 }
